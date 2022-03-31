aefe63d413e8ea7f95a9932d9f2237494c95eb4e
esawada <sawadaeduardo@gmail.com>
2022-03-31 18:54:29 -0300

    SpringBoot

---
+++ src/test/java/com/example/SpaceIMT/SpaceImtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpaceIMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceImtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
