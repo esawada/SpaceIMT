a25f7d1246c8076ca92f114b08bd08d6c8f09acf
Guilherme Toffanelli <104535116+GuiGitHubson@users.noreply.github.com>
2022-06-01 01:51:31 -0300

    Add files via upload

---
+++ Conquista.java
@@ -0,0 +1,87 @@
+public class Conquista{
+  
+    //Conquista por Tempo = int
+    //Conquista por Streak = Boolean
+
+    int Trovão_1 = 1;
+    int Trovão_2 = 2;
+    int Trovão_3 = 3;
+    boolean Criador_dos_comandos = true;
+
+    int Imparável_1 = 4;
+    int Imparável_2 = 5;
+    int Imparável_3 = 6;
+    boolean Máquina = true;
+
+    int Tá_de_Hack_1 = 7;
+    int Tá_de_Hack_2 = 8;
+    int Tá_de_Hack_3 = 9;
+    boolean A_Própria_IA = true;
+
+    int CPU_Humana_1 = 10;
+    int CPU_Humana_2 = 11;
+    int CPU_Humana_3 = 12;
+    boolean Onisciente = true;
+
+    public static void main(String[] args) {
+        if(1==1) {
+    System.out.println("Conquista desbloqueada: Trovão 1");
+    }
+        if(2==2) {
+    System.out.println("Conquista desbloqueada: Trovão 2");
+    }
+        if(3==3) {
+    System.out.println("Conquista desbloqueada: Trovão 3");
+    }
+        if(1==1 & 2==2 & 3==3) {
+    System.out.println("Conquista desbloqueada: Criador dos Comandos");
+    }
+        if(4==4) {
+    System.out.println("Conquista desbloqueada: Imparável 1");
+    }
+        if(5==5) {
+    System.out.println("Conquista desbloqueada: Imparável 2");
+    }
+        if(6==6) {
+    System.out.println("Conquista desbloqueada: Imparável 3");
+    }
+        if(4==4 & 5==5 & 6==6) {
+    System.out.println("Conquista desbloqueada: Máquina");
+    }
+        if(7==7) {
+    System.out.println("Conquista desbloqueada: Tá de Hack 1");
+    }
+        if(8==8) {
+    System.out.println("Conquista desbloqueada: Tá de Hack 2");
+    }
+        if(9==9) {
+    System.out.println("Conquista desbloqueada: Tá de Hack 3");
+    }
+        if(7==7 & 8==8 & 9==9) {
+    System.out.println("Conquista desbloqueada: A Própria IA");
+    }
+        if(10==10) {
+    System.out.println("Conquista desbloqueada: CPU Humana 1");
+    }
+        if(11==11) {
+    System.out.println("Conquista desbloqueada: CPU Humana 2");
+    }
+        if(12==12) {
+    System.out.println("Conquista desbloqueada: CPU Humana 3");
+    }
+        if(10==10 & 11==11 & 12==12) {
+    System.out.println("Conquista desbloqueada: Onisciente");
+        }
+
+
+
+
+
+    }                                                                                                              }                                                          
+
+
+
+
+    
+
+  
